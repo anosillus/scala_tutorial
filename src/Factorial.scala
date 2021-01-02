8152836c820dc3f2e1f20a05b91adb24d4042032
anosillus <anosillus@gmail.com>
2021-01-03 02:34:35 +0900

    2 chapter

---
+++ src/Factorial.scala
@@ -0,0 +1,9 @@
+import scala.math.BigInt
+
+object Factorial extends App {
+  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
+
+  println(factorial(1000))
+  println(factorial(1))
+
+}
---
+++ src/Factorization.scala
@@ -0,0 +1,9 @@
+import scala.math.sqrt
+
+object Factorization extends App {
+  var target = 24
+  var maxDivisor = sqrt(target).toInt
+
+  // println(target)
+  println(maxDivisor)
+}
